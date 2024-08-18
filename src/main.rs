e4659b30ec7a73a4c6cdfecbfafefc6b58273755
dcode2100 <dikshantsolanki214@gmail.com>
2024-08-18 08:29:34 +0530

    implemented functions and conditionals

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+// ! Function implementation
+
+
 fn main() {
-    println!("Hello, world!");
-}
+    let mut num: i32 = 3;
+    
+    for _i in 0..5 {
+            num = num + 1;
+        println!("{}", num);   
+        }
+        
+        if num > 5 {
+            println!("It is really greater then 5 and the value is {}",num);
+        }
+        else{
+            println!("It is not {}",num)
+        }
+        addition (3,num);
+        addition (2,num);
+
+        let mut _data_owning_string: String = String::from("The data_owning_string is heap stored, mutable, owned by declaration ");
+
+        let _string_slice: &str = "string_slice is reffrenced saved and is also used for borrowing part of string";
+    }
+    
+   
+    
+    pub fn addition (a:i32,b:i32) {
+        println!("The Sum is {}",a+b);
+    }
+    
+// ! String implementation
+// fn main() {
+
+//     let name: String = String::from("John"); // This way of initialization is known as a string literal
+//     let string2: &str = "Hello, world!"; // This way of initialization is known as a string slice
+//     println!("Hello, {}!", name);
+//     println!("Hello, {}!", string2);
+
+
+//    // Since the char1 at index 1000 might not be char so printing using println! will not work
+//    // So we need to use match to check if the char is Some or None
+//    // If the char is Some then we can print it
+//    // If the char is None then we can print a message
+//    // ! This is how rust save us from static type checking for char in future tense
+//     let char1: Option<char> = string2.chars().nth(1000);
+
+//     match char1 {
+//         Some(c) => println!("The char is {}", c),
+//         None => println!("The char is None"),
+//     }
+    
+
+// }
+
+// ! IF else implementation
+    // println!("Hello, world!");
+    // let is_male: bool = true;
+    // let is_female: bool = false;
+    // let is_above_18: bool = true;
+
+    // if is_male{
+    //     println!("You are a man");
+    // }
+    // else if is_female && is_above_18{
+    //     println!("You are a woman is above 18");
+    // }
+// }
